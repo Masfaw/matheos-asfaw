a97f714e4ace9b588787622f8bdc9f6fed22dbab
Matheos <matheos.asfaw@gmail.com>
2020-01-14 21:58:51 -0800

    implemented seek behaviour

--- Autonomous-Agents/models/vehicle.js
+++ Autonomous-Agents/models/vehicle.js
@@ -3,9 +3,33 @@ class Vehicle {
         this.position = createVector(x, y);
         this.acceleration = createVector(0, 0);
         this.velocity = createVector(0, 0);
+        this.maxSpeed = 5;
+        this.maxForce = 2;
     }
 
     applyForce = force => {
         this.acceleration.add(force);
     };
+
+    update = () => {
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+        this.position.add(this.velocity);
+        this.acceleration.set(0, 0);
+    }
+
+    seek = (target) => {
+        // steering = desired - velocity 
+        let desired = p5.Vector.sub(target, this.position);
+        let steering = p5.Vector.sub(desired, this.velocity);
+        steering.limit(this.maxForce);
+        this.applyForce(steering);
+    }
+
+    display = () => {
+        fill(255, 150);
+        noStroke();
+        // stroke(255);
+        ellipse(this.position.x, this.position.y, 24, 24);
+    }
 }
--- Autonomous-Agents/sketch.js
+++ Autonomous-Agents/sketch.js
@@ -1,5 +1,21 @@
+let vehicle;
+
 function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    vehicle = new Vehicle(320, 180);
     console.log("Hello There Matheos");
+    // background(51);
+
 }
 
-function draw() {}
+function draw() {
+    background(51);
+
+    let target = createVector(mouseX, mouseY);
+
+    vehicle.seek(target);
+
+
+    vehicle.update();
+    vehicle.display();
+}
