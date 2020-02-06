2cecb2a2827cdeda9b23e4df0b7ecd4e200d9264
Matheos <matheos.asfaw@gmail.com>
2020-02-05 21:46:01 -0800

    added unlimited power

---
+++ UNLIMITED-POWER/models/flowfeild.js
@@ -0,0 +1,57 @@
+class FlowField {
+    constructor(resolution) {
+        this.resolution = resolution;
+        this.cols = window.innerWidth / this.resolution;
+        this.rows = window.innerHeight / this.resolution;
+        let util = new Util();
+        this.field = util.make2dArray(this.cols);
+        this.initFeild();
+    }
+
+    initFeild() {
+        let mult = random(1.5, 3);
+        console.log("MULT", mult);
+        let seed = Math.floor(random(10000));
+        console.log("seed", seed)
+        noiseSeed(seed);
+        let xoff = 0;
+        for (let i = 0; i < this.cols; i++) {
+            let yoff = 0;
+            for (let j = 0; j < this.rows; j++) {
+                let theta = map(noise(xoff, yoff), 0, 1, 0, TWO_PI * mult);
+                this.field[i][j] = createVector(cos(theta), sin(theta));
+                yoff += 0.01;
+            }
+            xoff += 0.01;
+        }
+    }
+
+    lookup(lookup) {
+        let column = Math.floor(constrain(lookup.x / this.resolution, 0, this.cols - 1));
+        let row = Math.floor(constrain(lookup.y / this.resolution, 0, this.rows - 1));
+        return this.field[column][row].copy();
+    }
+    displayFlowFeild() {
+        for (let i = 0; i < this.cols; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                this.drawVector(
+                    this.field[i][j],
+                    i * this.resolution,
+                    j * this.resolution,
+                    this.resolution - 2
+                );
+            }
+        }
+    }
+
+    drawVector(v, x, y, scale) {
+        push();
+        let arrowSize = 4;
+        translate(x, y);
+        stroke(50, 100);
+        rotate(v.heading());
+        let len = v.mag() * scale;
+        line(0, 0, len, 0);
+        pop();
+    }
+}
---
+++ UNLIMITED-POWER/sketch.js
@@ -0,0 +1,24 @@
+let flowFeild;
+let vehicles = [];
+function setup() {
+    let canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.position(0, 0);
+    // vehicle = new Vehicle(320, 180);
+    flowFeild = new FlowField(2);
+    background(0);
+    // flowFeild.displayFlowFeild();
+
+    for (let i = 0; i < 5000; i++) {
+        let randomLocation = createVector(Math.floor(random(0, window.innerWidth)), Math.floor(random(0, window.innerHeight)));
+        vehicles.push(new Vehicle(randomLocation.x, randomLocation.y));
+    }
+    document.body.style.overflow = 'hidden';
+    frameRate(30);
+}
+
+function draw() {
+    vehicles.forEach(vehicle => {
+        vehicle.follow(flowFeild);
+        vehicle.run();
+    });
+}
