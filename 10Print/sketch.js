01a0cd39df30075fb21dbef63faed75ad6d67e48
Matheos <matheos.asfaw@gmail.com>
2020-08-02 19:59:58 -0700

    10 print sketch

---
+++ 10Print/sketch.js
@@ -0,0 +1,36 @@
+
+let resolution = 20;
+let cols = Math.floor(window.innerWidth / resolution);
+let rows = Math.floor(window.innerHeight / resolution);
+let threashold = Math.random();
+
+function setup() {
+    console.log({ threashold });
+
+    let canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.position(0, 0);
+    background(255);
+    for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
+            let locX = x * resolution;
+            let locY = y * resolution;
+            if (random(1) < threashold) {
+                line(locX, locY, locX + resolution, locY + resolution);
+            } else {
+                line(locX, locY + resolution, locX + resolution, locY);
+            }
+        }
+    }
+
+}
+
+
+
+function draw() {
+
+}
+
+function mouseClicked() {
+    console.log("Hello there 1 ", width, window.innerWidth)
+    console.log("Hello there 2 ", { cols, rows })
+}
