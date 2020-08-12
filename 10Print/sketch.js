27d4dbbafc6687ff810983294d75b1873a1b7b49
Matheos <matheos.asfaw@gmail.com>
2020-08-11 17:46:07 -0700

    finished 10 print

--- 10Print/sketch.js
+++ 10Print/sketch.js
@@ -2,10 +2,10 @@
 let resolution = 20;
 let cols = Math.floor(window.innerWidth / resolution);
 let rows = Math.floor(window.innerHeight / resolution);
-let threashold = Math.random();
+let threshold = Math.random();
 
 function setup() {
-    console.log({ threashold });
+    console.log({ threashold: threshold });
 
     let canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.position(0, 0);
@@ -14,7 +14,7 @@ function setup() {
         for (let y = 0; y < rows; y++) {
             let locX = x * resolution;
             let locY = y * resolution;
-            if (random(1) < threashold) {
+            if (random(1) < threshold) {
                 line(locX, locY, locX + resolution, locY + resolution);
             } else {
                 line(locX, locY + resolution, locX + resolution, locY);
