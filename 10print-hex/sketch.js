7881f9b2624d93dd2ccdade9fe7aa83413397ae0
matheos <matheos.asfaw@gmail.com>
2022-02-05 00:23:16 -0800

    small changes to 10 hex

--- 10print-hex/sketch.js
+++ 10print-hex/sketch.js
@@ -3,7 +3,10 @@ let zoff = 0;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    flowFeild = new FlowField(15);
+    let largerSide = window.innerWidth > window.innerHeight ? windowWidth : window.innerHeight;
+    let gridSizeONLargerAxis = Math.floor(largerSide/ 150); // this is because i want 100 to be the standard grid size
+    console.log({gridSizeONLargerAxis});
+    flowFeild = new FlowField(gridSizeONLargerAxis);
 }
 
 function draw() {
