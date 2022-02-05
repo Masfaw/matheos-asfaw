1c1a6e53249bb1ed9880e384e4dfb89dcd6a28b0
matheos <matheos.asfaw@gmail.com>
2022-02-05 00:23:36 -0800

    initial implementaion for quad tree

--- util/quadTree.js
+++ util/quadTree.js
@@ -1,4 +1,5 @@
 
+const CAPACITY = 4;
 
 class QuadTreePoint {
     constructor(x, y) {
@@ -7,12 +8,12 @@ class QuadTreePoint {
     }
 }
 
-class QuadTreeRectBoundary {
+class QuadTreeRectBoundary { // this is for boundary boxes drawn from the center
 
     constructor(x, y, w, h) {
-        this.xLoc = x;
-        this.yLoc = y;
-        this.width = w;
+        this.xLoc = x; // x location of the center of the box
+        this.yLoc = y; // y location of the center of the box
+        this.width = w; // this is actually the distance form center to side of the boundary
         this.height = h;
     }
 
@@ -24,6 +25,13 @@ class QuadTreeRectBoundary {
             other.yLoc + other.height < this.yLoc - this.height
         );
     }
+
+    contains(point) {
+        return point.xLoc <= this.xLoc + this.width && 
+                point.xLoc >= this.xLoc - this.width && 
+                point.yLoc <= this.yLoc + this.height && 
+                point.yLoc >= this.yLoc - this.height;
+    }
 }
 
 class QuadTree {
@@ -32,18 +40,69 @@ class QuadTree {
         this.capacity = capacity;
         this.points = [];
         this.hasSplit = false;
+        this.northWest
     }
 
     insert(point) {
-
+        if (!this.boundary.contains(point)) {
+            return false;
+          }
+      
+          if (this.points.length < this.capacity) {
+            this.points.push(point);
+            return true;
+          } else {
+            if (!this.divided) {
+              this.subdivide();
+            }
+            if (this.northeast.insert(point)) {
+              return true;
+            } else if (this.northwest.insert(point)) {
+              return true;
+            } else if (this.southeast.insert(point)) {
+              return true;
+            } else if (this.southwest.insert(point)) {
+              return true;
+            }
+          }
     }
 
     subdivide() {
-
+        let x = this.boundary.x;
+        let y = this.boundary.y;
+        let w = this.boundary.w;
+        let h = this.boundary.h;
+        let ne = new QuadTreeRectBoundary(x + w / 2, y - h / 2, w / 2, h / 2);
+        this.northeast = new QuadTree(ne, this.capacity);
+        let nw = new QuadTreeRectBoundary(x - w / 2, y - h / 2, w / 2, h / 2);
+        this.northwest = new QuadTree(nw, this.capacity);
+        let se = new QuadTreeRectBoundary(x + w / 2, y + h / 2, w / 2, h / 2);
+        this.southeast = new QuadTree(se, this.capacity);
+        let sw = new QuadTreeRectBoundary(x - w / 2, y + h / 2, w / 2, h / 2);
+        this.southwest = new QuadTree(sw, this.capacity);
+        this.divided = true
     }
 
-    query(range) {
-
+    query(range,found) {
+        if (!found) {
+            found = [];
+          }
+          if (!this.boundary.intersects(range)) {
+            return;
+          } else {
+            for (let p of this.points) {
+              if (range.contains(p)) {
+                found.push(p);
+              }
+            }
+            if (this.divided) {
+              this.northwest.query(range, found);
+              this.northeast.query(range, found);
+              this.southwest.query(range, found);
+              this.southeast.query(range, found);
+            }
+          }
+          return found;
     }
 
     render() {
@@ -54,6 +113,19 @@ class QuadTree {
         rect(
             this.boundary.xLoc,
             this.boundary.yLoc,
+            this.boundary.w * 2,
+            this.boundary.h * 2
         )
+        for (let p of this.points) {
+            strokeWeight(2);
+            point(p.x, p.y);
+          }
+
+        if (this.divided) {
+        this.northeast.show();
+        this.northwest.show();
+        this.southeast.show();
+        this.southwest.show();
+        }
     }
 }
